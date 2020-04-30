86523e02121febb2b40b1e1e1b44d2176159f4a9
mio.shibata <misi82863@gmail.com>
2020-04-30 08:44:35 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,13 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :destroy]
+  
   def index
-    @tasks = Task.all
+    if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
   end
 
   def show
@@ -12,11 +18,10 @@ class TasksController < ApplicationController
   end
 
   def create
-     @task = Task.new(task_params)
-
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'タスク が正常に作成されました'
-      redirect_to task_path(@task)
+      redirect_to @task
     else
       flash.now[:danger] = 'タスク が作成されませんでした'
       render :new
@@ -56,7 +61,13 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content, :status)
   end
- 
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
  
   
 end
