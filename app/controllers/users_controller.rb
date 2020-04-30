ad409a842e8f89e3b1a3b57b13d328261511fa86
mio.shibata <misi82863@gmail.com>
2020-04-30 11:25:17 +0000

    user modify

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,11 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in , only: [:index, :show]
-  def index
-    @users = User.order(id: :desc).page(params[:page]).per(25)
-  end
-
-  def show
-    @user = User.find(params[:id])
-    @tasks = @user.tasks.order(id: :desc).page(params[:page])
-    counts(@user)
-  end
-
+  
   def new
     @user = User.new
   end
 
   def create
- @user = User.new(user_params)
+    @user = User.new(user_params)
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
